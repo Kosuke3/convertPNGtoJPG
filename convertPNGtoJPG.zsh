e343058611702e875d060eb24e69a732793a1f83
Kosuke Fujita <kos_mail+GitHub@mac.com>
2025-06-29 21:23:09 +0900

    スクリプトおよびREADMEのアップデート、CCHANGELOGの追加

--- convertPNGtoJPG.zsh
+++ convertPNGtoJPG.zsh
@@ -8,15 +8,40 @@ if [[ -z "$1" ]]; then
 exit 1
 fi
 
-# 再帰的にPNGファイルを探してJPG画質80/100に変換
+# 再帰的にPNGファイルを検索
 find "$1" -type f -iname '*.png' | while IFS= read -r pngfile; do
  jpgfile="${pngfile%.*}.jpg"
- echo "変換中：$pngfile→$jpgfile"
  
- if magick "$pngfile" -quality 80 "$jpgfile"; then
-  echo "✅ 変換成功、削除: $pngfile"
-  rm "$pngfile"
+ # PNGをJPGへ変換し、量ファイルのサイズを比較
+ magick "$pngfile" -quality 80 "$jpgfile"
+ 
+ if [[ -f "$jpgfile" ]]; then
+  pngsize=$(stat -f%z "$pngfile")
+  jpgsize=$(stat -f%z "$jpgfile")
+  
+  # 画面表示用ファイルサイズを変換
+  png_human=$(awk -v s="$pngsize" 'BEGIN {
+      if (s < 1024) printf "%.0f butes", s;
+      else if (s < 1048576) printf "%.1f kB", s/1024;
+      else printf "%.2f MB", s/1048576;
+  }')
+  
+  jpg_human=$(awk -v s="$jpgsize" 'BEGIN {
+      if (s < 1024) printf "%.0f bytes", s;
+      else if (s < 1045576) printf "%.1f kB", s/1024;
+      else printf "%.2f MB", s/1048576;
+  }')
+  
+   # 容量の小さいファイルを残し、大きなファイルを削除する
+   if (( jpgsize < pngsize)); then
+    deletefile="$pngfile"
+    else
+    deletefile="$jpgfile"
+   fi
+   
+   echo "📊変換結果：$pngfile ${png_human} 　$(basename "$jpgfile") ${jpg_human} bytes → $(basename "$deletefile")を削除"
+   rm "$deletefile"
  else
-  echo "❌️ 変換失敗：$pngfile"
+  echo "⚠️ JPG生成失敗：$jpgfile"
  fi
 done
